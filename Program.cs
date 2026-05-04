657c8c51574e48665a219e80ee674c3186652a8e
Alex Belov <belovtt@gmail.com>
2026-05-04 17:29:37 -0400

    SRP

--- Program.cs
+++ Program.cs
@@ -1,1 +1,24 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using static SOLID.SingleResposibility;
+using static SOLID.SingleResposibility.InvoiceGenerator;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Started!");
+        //Console.ReadLine();
+
+        // 1. Instantiate the dependencies
+        var generator = new InvoiceGenerator();
+        var repository = new InvoiceRepository();
+        var emailSender = new InvoiceEmailSender();
+
+        // 2. Inject them into the service
+        var invoiceService = new InvoiceService(generator, repository, emailSender);
+
+        // 3. Execute the logic
+        invoiceService.ProcessInvoice();
+
+    }
+}
---
+++ SingleResposibility.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using System.Text;
+using static SOLID.SingleResposibility;
+using static SOLID.SingleResposibility.InvoiceGenerator;
+
+namespace SOLID
+{
+    public class SingleResposibility
+    {
+        // VIOLATION: This class does too much.
+        public class Invoice
+        {
+            public void GenerateInvoice()
+            {
+                // Code to generate the invoice
+            }
+
+            public void SaveToDatabase()
+            {
+                // Code to save invoice data to the database
+            }
+
+            public void SendEmail()
+            {
+                // Code to send the invoice via email
+            }
+        }
+        //Good Code ✅
+        // Step 1: Separate the Responsibilities
+        public class InvoiceGenerator
+        {
+            public void GenerateInvoice()
+            {
+                // Code to generate the invoice
+                Console.WriteLine("Generate Invoice!");
+                //Console.ReadLine();
+            }
+            public class InvoiceRepository
+            {
+                public void SaveToDatabase()
+                {
+                    // Code to save invoice data to the database
+                    Console.WriteLine("Save Invoice To Database!");
+                    //Console.ReadLine();
+                }
+            }
+            public class InvoiceEmailSender
+            {
+                public void SendEmail()
+                {
+                    // Code to send the invoice via email
+                    Console.WriteLine("Email Sent!");
+                    //Console.ReadLine();
+                }
+            }
+
+
+
+        }
+
+
+        //Step 2: Coordinating with a Higher-Level Class
+        public class InvoiceService
+        {
+            private readonly InvoiceGenerator _invoiceGenerator;
+            private readonly InvoiceRepository _invoiceRepository;
+            private readonly InvoiceEmailSender _invoiceEmailSender;
+
+            public InvoiceService(
+                InvoiceGenerator invoiceGenerator,
+                InvoiceRepository invoiceRepository,
+                InvoiceEmailSender invoiceEmailSender)
+            {
+                _invoiceGenerator = invoiceGenerator;
+                _invoiceRepository = invoiceRepository;
+                _invoiceEmailSender = invoiceEmailSender;
+            }
+
+            public void ProcessInvoice()
+            {
+                _invoiceGenerator.GenerateInvoice();
+                _invoiceRepository.SaveToDatabase();
+                _invoiceEmailSender.SendEmail();
+            }
+        }
+    }
+   
+
+}
